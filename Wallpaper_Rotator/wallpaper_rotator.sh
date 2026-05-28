7d34ed54db00e6a0be743d6a7b9d99554b4bf96e
Utkarsh Pankaj <133890893+utkarshpankaj5@users.noreply.github.com>
2026-05-28 22:17:39 +0530

    Update wallpaper_rotator.sh

--- Wallpaper_Rotator/wallpaper_rotator.sh
+++ Wallpaper_Rotator/wallpaper_rotator.sh
@@ -4,7 +4,7 @@ WALLPAPER_DIR="/home/utkarsh/Downloads/Wallpaper"
 STATE_FILE="/tmp/current_wallpaper"
 TOTAL=8
 
-# Create counter file if missing
+
 if [ ! -f "$STATE_FILE" ]; then
     echo 1 > "$STATE_FILE"
 fi
@@ -13,16 +13,14 @@ CURRENT=$(cat "$STATE_FILE")
 
 WALL="$WALLPAPER_DIR/wall$CURRENT.png"
 
-# Change wallpaper
+
 gsettings set org.gnome.desktop.background picture-uri "file://$WALL"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALL"
 
 echo "Wallpaper changed to wall$CURRENT.png"
 
-# Increment
 NEXT=$((CURRENT + 1))
 
-# Reset to 1 if exceeded
 if [ "$NEXT" -gt "$TOTAL" ]; then
     NEXT=1
 fi
